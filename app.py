1ee5e3c022e7f0522541386624b93229fc0f7584
aseelmi-lab <aseel.mashall@gmail.com>
2026-06-10 14:12:33 +0300

    تجهيز للنشر على Render

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ if __name__ == '__main__':
         print("="*60 + "\n")
     else:
         print("\n✅ تم تحميل المفتاح بنجاح. التطبيق جاهز!\n")
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
